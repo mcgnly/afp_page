23d814a9923f19c7a445006ec21f00bab435737d
mcgnly <katie@mcgnly.com>
2016-01-24 16:41:45 +0100

    get rid of a lot of login-related stuff in routes change html to fix up what displays

--- app.py
+++ app.py
@@ -36,32 +36,6 @@ def home():
 def welcome():
 	return render_template ("wtf.html")
 
-# route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-        	session['logged_in'] = True
-        	flash('you were just logged in')
-        	return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-	session.pop('logged_in', None)
-	flash('you logged out')
-	return redirect(url_for('welcome'))
-
-
-#this was for straight sqlight, don't need it for sqlalchemy
-# def connect_db():
-#     return sqlite3.connect(app.database)
-
-
-
 #start the server with 'run()' method
 if __name__ == '__main__':
 	app.run(debug=True)
